53dea1d0394dbe80281b7aac54472222476cc3c0
jime <kuritahajime.business@gmail.com>
2024-08-08 02:15:08 +0900

    php化初歩

---
+++ index.php
@@ -0,0 +1,163 @@
+<!DOCTYPE html>
+<html lang="jp">
+    <head>
+        <meta charset="UTF-8">
+        <title>栗田孟 ポートフォリオ</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="css/slick.css">
+        <link rel="stylesheet" href="css/slick-theme.css">
+    </head>
+
+    <body>
+        
+        <div class="header">
+            <div class="header-content">
+                <div class="header-links">
+                    <a class="header-logo"><img class="header-img" src="img/header-logo.png" alt="" ></a>
+                    <a class="header-link">HOME</a>
+                    <a class="header-link">NEWS</a>
+                    <a class="header-link">COMPANY</a>
+                    <a class="header-link">SERVICE</a>
+                    <a class="header-link">RECRUIT</a>
+                    <a class="header-link">ACCESS</a>
+                    <a class="header-link header-link-last">CONTACT</a>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="top-page">
+            <div class="top-page-contents">
+                <div class="top-page-image">
+                    <ul class="slider">
+                        <li><a href="#"><img src="img/codeing.jpg" alt="image01"></a></li>
+                        <li><a href="#"><img src="img/enginer.jpg" alt="image03"></a></li>
+                        <li><a href="#"><img src="img/enginer3.jpg" alt="image04"></a></li>
+                    </ul>
+                    <div class="slider-coment">
+                        <h1>give  form  to  ideas</h1>
+                        <h3>アイディアをカタチに</h3>
+                    </div>
+                </div>
+
+                <div class="top-page-greeting">
+                    <h2>ご挨拶</h2>
+                    <h3 class="title">Greeting</h3>
+                    <p>株式会社 Jime Companyは、ソフトフェアソフトウェア開発事業を通じて</p>
+                    <p class="greeting-margin">社会の発展に貢献することをミッションとしています</p>
+                    <p>クラウドでの運用を想定したアプリケーションの企画・開発から運用保守までワンストップで対応します。</p>
+                    <p>ゲーム・大手企業様業務システムなど多くの開発実績で培った技術で安定したサービスを作り上げます。</p>
+                </div>
+
+                <div class="top-page-service">
+                    <h2>主なサービス</h2>
+                    <h3 class="title">Service</h3>
+                    <div class="service-wrapper">
+                        <div class="service-wrapper-content">
+                            <a href=""><img src="img/codeing.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div>
+                        <div class="service-wrapper-content">
+                            <a href=""><img src="img/codeing2.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div>
+                        <div class="service-wrapper-content">
+                            <a href=""><img src="img/enginer2.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div>
+
+                        <div class="service-wrapper-content">
+                            <a href=""><img src="img/enginer3.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div>
+                        <div class="service-wrapper-content">
+                            <a href=""><img src="img/codeing.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div>
+                        <!-- <div class="service-wrapper-content">
+                            <a href=""><img src="img/codeing2.jpg" alt=""></a>
+                            <a class="u-cent">受託開発サービス</a>
+                        </div> -->
+                    </div>
+                    <div class="top-page-btn">
+                        <a href="">サービス詳細</a>
+                    </div>
+                </div>
+
+                <div class="top-page-info">
+                    <h2>新着情報</h2>
+                    <h3 class="title">News</h3>
+                    <div class="news-wrapper">
+                        <div class="news-contents">
+                            <div class="news">
+                                <p class="news-left"></p>
+                                <p class="news-right"></p>
+                            </div>
+
+                            <div class="news">
+                                <p class="news-left"></p>
+                                <p class="news-right"></p>
+                            </div>
+
+                            <div class="news">
+                                <p class="news-left"></p>
+                                <p class="news-right"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="top-page-btn">
+                        <a href="">サービス詳細</a>
+                    </div>
+                </div>
+
+                <div class="top-page-acsses">
+                    <h2>アクセス</h2>
+                    <h3 class="title">Access</h3>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15072.904166446431!2d139.6927172081058!3d35.67996024192092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188cd0d6b1ba1f%3A0x1c32a1f1ecacfdd5!2z5paw5a6_6aeF!5e0!3m2!1sja!2sjp!4v1718897973523!5m2!1sja!2sjp" width="800" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+
+                <div class="top-page-recluting">
+                    <h2>採用情報</h2>
+                    <h3 class="title">Recruit</h3>
+                    <div class="recluting-contents">
+                        <p>この度サイトのリニューアルと事業拡大をもとに、</p>
+                        <p>新たなメンバーを募集します。</p>
+                        <p>社員の個性を尊重し、誰もが活躍できる環境作りを大切にしている会社の想いや</p>
+                        <p>価値観に共感し、スキル＆キャリアアップへ、会社と共に成長したい、</p>
+                        <p>という方をお待ちしております。</p>
+                    </div>
+                    <div class="top-page-btn">
+                        <a href="">採用情報はこちら</a>
+                    </div>
+                </div>
+
+                <div class="top-page-partner">
+                    <h2>パートナー企業募集中</h2>
+                    <h3 class="title-partner">PARTNER</h3>
+                    <div class="partner-contents">
+                        <p>株式会社 Jime Companyでは、ビジネスパートナー企業を募集中です。</p>
+                        <p>詳細は、下記お問い合わせフォームよりお問い合わせください。</p>
+                    </div>
+                    <div class="partner-btn">
+                        <a href="">お問い合わせはこちら</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="footer">
+            <div class="footer-content" >
+                <img src="img/footer-logo.png" alt="">
+                <p>〒000-0000  東京都板橋区〇〇〇〇 〇〇〇〇ビル7F</p>
+                <a href="">・個人情報保護方針</a>
+            </div>
+        </div>
+
+        <!-- JAVASCRIPTS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script src="js/slick.js"></script>
+        <script src="js/slick.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
